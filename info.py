cb2f6a6e00657d9ca6465cee7be7e592d33c42ba
audibleyeehaw <146049814+audibleyeehaw@users.noreply.github.com>
2024-11-27 13:35:18 -0500

    Add files via upload

---
+++ info.py
@@ -0,0 +1,77 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "images/peter-parker.jpg"
+about_me = "With great power, comes great responsiblility."
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://en.wikipedia.org/wiki/Peter_Parker_(Sam_Raimi_film_series)"
+my_github_url = "https://www.gettyimages.com/photos/tobey-maguire-spiderman"
+my_email_address = "p3terp@rk3r@gmail.com"
+
+
+education_data ={
+    'Degree': 'Biophysics',
+    'Institution': 'Columbia University',
+    'Location': 'New York City,NY',
+    'Graduation Date': 'N/A',
+    'GPA': 'Bad'
+}
+course_data = {
+    "code":["CHEM", "BIO", "PHYS", ], 
+    "names":["Chemistry", "Biology", "Physics"], 
+    "grade_recieved":["C", "B", "B"],
+    "skills":["I do good whem I'm there.", "Funny because my biology is really weird", "My whole thing is physics"]
+    }
+experience_data = {
+    "Photographer": (["- It pays the bills",
+                                                                          "- Spider-Man is not a villian, but JJ won't listen.", "- Made a couple of enemies unfortunately"],"images/dailybugle.webp"),
+    "Spider-Man":(["- Just your friendly neighborhood Spider-Man",
+                                                           "- Making Uncle Ben proud."],"images/spiderman.jpg"),
+    "Pizza boy":(["- More than 30 minutes, and your pizza's free!"],"images/pizza_time.webp")
+
+}
+
+projects_data = {
+    "Fixing my Relationship": "I love MJ and I have to work harder to show her",
+}
+
+programming_data = {
+    "Python": 90,
+    "Java": 70,
+    "C": 40,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {
+    "English": "🇬🇧",
+    "Ukranian":"🇺🇦"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Ukranian": "Insults"
+}
+leadership_data = {
+    "Member of Spider-team": (["- I always wanted brothers"],"images/peterparty.jpg"),
+
+}
+activity_data={
+    "Protecting the City": ["- Fighting bad guys", 
+            "- Helping law enforcement"],
+    "Photography": ["- Taking photos of Spider-Man","- Getting yelled at by J. Jonah. Jameson"]
+
+}
