e1a620deeb7ad5534a5d884ad21a55349aa73329
audibleyeehaw <146049814+audibleyeehaw@users.noreply.github.com>
2024-11-27 23:53:13 -0500

    Add files via upload

---
+++ Marvel_API.py
@@ -0,0 +1,5 @@
+description = "This page will generate a random auperhero based on your inputs!"
+
+supes = "Images/marvel_dc.jpg"
+
+images =  "httpss://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
